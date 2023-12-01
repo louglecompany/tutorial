3c6b2922ef639b2d3f7b2c12b153aa98ce7c550c
louglecompany <144390491+louglecompany@users.noreply.github.com>
2023-12-01 14:15:39 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,16 +13,6 @@ setTimeout(function () {
         $('.loader div').css('display', 'none');
     }, 100);
 
-    // Adiciona as classes 'show' e 'hidden' ao texto após a transição
-    $('.texto p').css('display', 'block'); // Exibe o texto
-    $('.texto').fadeIn(10).css('display', 'flex');
-    $('.texto p').animate({ fontSize: '0px' }, 10);
-
-    // Oculta o texto após a animação
-    setTimeout(function () {
-        $('.texto').css('display', 'none');
-    }, 2300);
-
 }, 2000); // Reduzi o tempo para 1 segundo para que o texto seja exibido imediatamente
 
 // script.js
@@ -52,30 +42,35 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(showElement, 2500);
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var botoesElement = document.querySelector('.botoes');
 
     function ajustarPosicaoTop() {
         // Verificar a largura da tela
         var larguraDaTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-        // Se a largura da tela for menor ou igual a 1024 pixels, ajustar o top para 200px
-        if (larguraDaTela <= 1024) {
-            botoesElement.style.top = "420px";
+        // Se a largura da tela for menor ou igual a 1280 pixels, ajustar o top para 200px
+        if (larguraDaTela <= 1280) {
+            botoesElement.style.top = "500px";
+        }
+        // Se a largura da tela estiver entre 1280 e 1440 pixels, ajustar o top para 1000px
+        else if (larguraDaTela > 1280 && larguraDaTela <= 1440) {
+            botoesElement.style.top = "500px";
         } else {
-            // Caso contrário, manter o top como 600px
+            // Caso contrário, manter o top como 0px
             botoesElement.style.top = "600px";
         }
     }
 
     // Exemplo: Aplicar showElement para os botões após 2 segundos
-    setTimeout(function() {
+    setTimeout(function () {
         ajustarPosicaoTop();
 
         // Adicionar um ouvinte de redimensionamento para ajustar a posição top quando a tela for redimensionada
         window.addEventListener('resize', ajustarPosicaoTop);
     }, 2000);
 });
+
 
 document.addEventListener("DOMContentLoaded", function() {
     var menuElement = document.querySelector('.menu');
@@ -85,8 +80,8 @@ document.addEventListener("DOMContentLoaded", function() {
         var larguraDaTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
         // Se a largura da tela for menor ou igual a 1024 pixels, ajustar o top para 80%
-        if (larguraDaTela <= 1024) {
-            menuElement.style.top = "70%";
+        if (larguraDaTela <= 1440) {
+            menuElement.style.top = "43%";
         } else {
             // Caso contrário, ajustar o top para 25%
             menuElement.style.top = "25%";
