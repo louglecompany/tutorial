c19df98692a185311a01d33ff165c0a24cba4e48
louglecompany <144390491+louglecompany@users.noreply.github.com>
2023-12-07 14:09:28 -0300

    Update script.js

---
+++ js/ajuda/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var btnInicio = document.getElementById('btnInicio');
+
+    // Adiciona um ouvinte de clique para o botão Início
+    btnInicio.addEventListener('click', function() {
+        // Redireciona para a página desejada no GitHub (substitua 'SEU-USUARIO' e 'SEU-REPOSITORIO' pelos seus dados)
+        window.location.href = '//index.html';
+    });
+});
