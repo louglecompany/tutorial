7d21fe4c3804c61429ed136c69ae31cda1b2a64a
louglecompany <144390491+louglecompany@users.noreply.github.com>
2023-12-08 15:17:40 -0300

    Add files via upload

---
+++ js/ajuda/contato/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var btnInicio = document.getElementById('btnInicio');
+
+    // Adiciona um ouvinte de clique para o botão Início
+    btnInicio.addEventListener('click', function() {
+        // Redireciona para a página desejada usando um caminho absoluto
+        window.location.href = '../../index.html';
+    });
+});
